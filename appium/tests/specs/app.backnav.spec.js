bf34f63d38d339f2a62c4730043c9041d1b3586b
Aviel <aviel@aviels-mbp.home>
2020-12-11 00:41:49 +0200

    appium demo

--- appium/tests/specs/app.backnav.spec.js
+++ appium/tests/specs/app.backnav.spec.js
@@ -19,8 +19,17 @@ describe('Backnavigation and app in app test', () => {
         await sleep(3000)
 
         bitcoinTile.click()
+        await sleep(2000)
+        const buyButton = await driver.$("~detail-screen-buy-button");
+        buyButton.click()
+        await sleep(2000)
+        const inpt = await driver.$("~detail-screen-modal-input");
+        console.log(inpt)
+        inpt.setValue(100)
+        await sleep(2000)
+        const submitButton = await driver.$("~detail-screen-modal-submit")
+        submitButton.click()
         await sleep(6000)
-
     });
 
 });
